020b99404f779f121af5b37242ab32b3765bd28c
Faisal <48144198+MahbubHossainFaisal@users.noreply.github.com>
2020-02-10 01:56:34 +0600

    Add files via upload

---
+++ show.php
@@ -0,0 +1,22 @@
+<?php
+	$x=$_GET['Name'];
+	echo "Welcome $x";
+	echo "</br>";
+	$y=$_GET['Email'];
+	echo "Your mail is $y";
+	echo "</br>";
+	$z=$_GET['Birthdate'];
+	echo "Your Birthdate is $z";
+	echo "</br>";
+	$a=$_GET['Gender'];
+	echo "You are a $a";
+	echo"</br>";
+	$b=$_GET['Degree'];
+	echo "You have completed $b";
+	echo "</br>";
+	$c=$_GET['Group'];
+	echo "Your blood group is $c";
+	echo "</br>";
+?>
+
+		?>
